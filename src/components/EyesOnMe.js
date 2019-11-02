7eb394bf14be167824136b6900aa678014216cb3
Tammy Metz <tammy@tammymetz.com>
2019-11-02 22:31:25 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -3,11 +3,11 @@ import React from 'react';
 class EyesOnMe extends React.Component {
 
   handleFocus = () => {
-    console.log('Good!')
+    console.log('Good!');
   }
 
   handleBlur = () => {
-    console.log('Hey! Eyes on me!')
+    console.log('Hey! Eyes on me!');
   }
 
   render() {
